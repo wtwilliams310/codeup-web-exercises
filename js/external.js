a3fcc0b9cedd402789c09c91879423999fdf6e3c
Titus Williams <wtwilliams310@gmail.com>
2022-11-21 16:55:12 -0600

    worked on functions exercises.

---
+++ js/external.js
@@ -0,0 +1,10 @@
+//#1 exercise
+// console.log("Hello World");
+//
+// alert("Welcome to my Website!");
+
+//# 2 exercise
+let pr = prompt("What is your favorite color?");
+alert(`Great ${pr} is my favorit color too!`);
+
+//# 3
