a86e2b92ef059c7ed7d1ceb31ed34d5a00db2173
Titus Williams <wtwilliams310@gmail.com>
2023-01-13 16:32:39 -0600

    des: completed mouse-events exercises

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,10 +1,33 @@
-$(function() {
-	alert('The DOM has finished loading the second time.')
+// $(function() {
+// 	alert('The DOM has finished loading the second time.')
+// })
+
+// $(() => {
+// 	alert('The DOM has finished loading the third time.')
+
+// })
+
+// window.onload(alert("like this"))
+
+
+var ele = document.getElementById('my-element');
+ele.addEventListener('click', function(e) {
+    console.log(e);
+});
+
+$(".h1-id").click(function(){
+	$(".h1-id").css('color', 'red')
 })
 
-$(() => {
-	alert('The DOM has finished loading the third time.')
+// $("#paragraph").dblclick(() => {
+// 	$("#paragraph").css('color', 'blue')
+// })
 
-})
-
-window.onload(alert("like this"))
+$('#paragraph').hover(
+    function() {
+        $(this).css('background-color', '#FF0');
+    },
+    function() {
+        $(this).css('background-color', '#FFF');
+    }
+);
