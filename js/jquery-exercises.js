465c247ca7455a5f4d1292beb27360cefacef0b3
Titus Williams <wtwilliams310@gmail.com>
2023-01-13 14:29:24 -0600

    des: initial files; completed jquery-Introduction

---
+++ js/jquery-exercises.js
@@ -0,0 +1,10 @@
+$(function() {
+	alert('The DOM has finished loading the second time.')
+})
+
+$(() => {
+	alert('The DOM has finished loading the third time.')
+
+})
+
+window.onload(alert("like this"))
